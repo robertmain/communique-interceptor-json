1194a0e2cbf60a362730859672ff3eec01b28503
Robert Main <lenswipe@gmail.com>
2015-10-05 00:10:44 +0100

    Create `DELETE` test
    Create `DELETE` unit test for JSON interceptor

--- test/JSONTest.php
+++ test/JSONTest.php
@@ -12,4 +12,12 @@ class JSONTest extends PHPUnit_Framework_TestCase{
 		PHPUnit_Framework_TestCase::assertEquals(array('request' => 'payload'), $processed_request->payload);
 		PHPUnit_Framework_TestCase::assertEquals($request, $processed_request);
     }
+
+    public function test_delete_request(){
+    	$request = new \Communique\RESTClientRequest('DELETE', 'http://domain.com/users', array('request' => 'payload'));
+    	$processed_request = $this->JSON->request($request);
+    	PHPUnit_Framework_TestCase::assertEquals($request->headers['Accept'], 'application/json');
+		PHPUnit_Framework_TestCase::assertEquals(array('request' => 'payload'), $processed_request->payload);
+		PHPUnit_Framework_TestCase::assertEquals($request, $processed_request);
+    }
 }
