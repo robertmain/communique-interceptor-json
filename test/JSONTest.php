9203ad5dbffe4c64ab0d1f14b78086ed445955dd
Robert Main <lenswipe@gmail.com>
2015-10-05 00:10:10 +0100

    Create test for `GET` request
    Createa unit test for `GET` requests.

---
+++ test/JSONTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class JSONTest extends PHPUnit_Framework_TestCase{
+	public function setUp(){
+		$this->JSON = new \Communique\Interceptors\JSON\JSON();
+	}
+
+    public function test_get_request(){
+    	$request = new \Communique\RESTClientRequest('GET', 'http://domain.com/users', array('request' => 'payload'));
+    	$processed_request = $this->JSON->request($request);
+    	PHPUnit_Framework_TestCase::assertEquals($request->headers['Accept'], 'application/json');
+		PHPUnit_Framework_TestCase::assertEquals(array('request' => 'payload'), $processed_request->payload);
+		PHPUnit_Framework_TestCase::assertEquals($request, $processed_request);
+    }
+}
