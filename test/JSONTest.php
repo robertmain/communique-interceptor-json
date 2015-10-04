17e55d3f79f4e68f091995add598cedb863f3297
Robert Main <lenswipe@gmail.com>
2015-10-05 00:11:47 +0100

    Create `PUT` test
    Create test for `PUT` requests

--- test/JSONTest.php
+++ test/JSONTest.php
@@ -20,4 +20,13 @@ class JSONTest extends PHPUnit_Framework_TestCase{
 		PHPUnit_Framework_TestCase::assertEquals(array('request' => 'payload'), $processed_request->payload);
 		PHPUnit_Framework_TestCase::assertEquals($request, $processed_request);
     }
+
+    public function test_put_request(){
+    	$request = new \Communique\RESTClientRequest('PUT', 'http://domain.com/users', array('request' => 'payload'));
+    	$processed_request = $this->JSON->request($request);
+    	PHPUnit_Framework_TestCase::assertEquals($request->headers['Accept'], 'application/json');
+    	PHPUnit_Framework_TestCase::assertEquals($request->headers['Content-Type'], 'application/json');
+    	PHPUnit_Framework_TestCase::assertJsonStringEqualsJsonString('{"request": "payload"}', $processed_request->payload);
+    	PHPUnit_Framework_TestCase::assertEquals($request, $processed_request);
+    }
 }
